a3ac8dfcbdcc8ae4128df7102b76f0cb77f4ce84
jlclemon <jlclemon@hotmail.com>
2018-03-11 14:47:03 -0500

    Update to the test_inference for having the boxes converted to pixel coordinates internally.  Still a basic test platform file

--- traffic_light_dl/workspace/test_inference.py
+++ traffic_light_dl/workspace/test_inference.py
@@ -36,61 +36,21 @@ PATH_TO_LABELS = os.path.join('data', 'bosch_traffic_light.pbtxt')
 
 NUM_CLASSES = 3
 
-def filter_boxes(min_score, boxes, scores, classes):
-    """Return boxes with a confidence >= `min_score`"""
-    n = len(classes)
-    idxs = []
-    for i in range(n):
-        if scores[i] >= min_score:
-            idxs.append(i)
-    
-    filtered_boxes = boxes[idxs, ...]
-    filtered_scores = scores[idxs, ...]
-    filtered_classes = classes[idxs, ...]
-    return filtered_boxes, filtered_scores, filtered_classes
-
-def to_image_coords(boxes, height, width):
-    """
-    The original box coordinate output is normalized, i.e [0, 1].
-
-    This converts it back to the original coordinate based on the image
-    size.
-    """
-    box_coords = np.zeros_like(boxes)
-    box_coords[:, 0] = boxes[:, 0] * height
-    box_coords[:, 1] = boxes[:, 1] * width
-    box_coords[:, 2] = boxes[:, 2] * height
-    box_coords[:, 3] = boxes[:, 3] * width
-
-    return box_coords
-def draw_bounding_box_on_image(image, bounding_box):
-    drawn_image = image.clone()
-
-    ymin,xmin,ymax, xmax = bounding_box
-    cv2.rectangle(drawn_image,(xmin,ymin),(xmax,ymax),(0,255,0),2)
-    cv2.putText(drawn_image,'Traffic Light',(x+w+10,y+h),0,0.3,(0,255,0))
-
-    return drawn_image
-
-def draw_boxes(image, boxes, classes, thickness=4):
-    # Colors (one for each class)
-    cmap = ImageColor.colormap
-    print("Number of colors =", len(cmap))
-    COLOR_LIST = sorted([c for c in cmap.keys()])
-
-    """Draw bounding boxes on the image"""
-    draw = ImageDraw.Draw(image)
-    for i in range(len(boxes)):
-        print('Drawing ', boxes [i,...])
-        bot, left, top, right = boxes[i, ...]
-        class_id = int(classes[i])
-        color = COLOR_LIST[class_id]
-        draw.line([(left, top), (left, bot), (right, bot), (right, top), (left, top)], width=thickness, fill=color)
 
 class DetectionInferenceEngine(object):
-    def __init__(self):
+    def __init__(self, confidence_thr= 0.02, class_dict= {}, color_list=None):
         self.detection_graph = tf.Graph()
         self.load_graph()
+        self.confidence_thr = confidence_thr
+        self.class_dict = class_dict
+
+        if(color_list == None):
+            self.color_list = ['white','green', 'yellow', 'red','gray']
+        else:
+            self.color_list = color_list
+
+
+
     def load_graph(self):
         with self.detection_graph.as_default():
             od_graph_def = tf.GraphDef()
@@ -123,8 +83,74 @@ class DetectionInferenceEngine(object):
             output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8)
             output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
             output_dict['detection_scores'] = output_dict['detection_scores'][0]
+ 
+            output_dict['detection_boxes'], output_dict['detection_scores'], output_dict['detection_classes'] = self.filter_boxes(self.confidence_thr, output_dict['detection_boxes'], output_dict['detection_scores'], output_dict['detection_classes'])
+
+ 
+            print('shape: ',image.shape)
+            height, width = image.shape[1:3]
+            output_dict['detection_boxes'] = self.to_image_coords(output_dict['detection_boxes'], height, width)
+
       
         return output_dict
+
+
+    def filter_boxes(self,min_score, boxes, scores, classes):
+        """Return boxes with a confidence >= `min_score`"""
+        n = len(classes)
+        idxs = []
+        for i in range(n):
+            if scores[i] >= min_score:
+                idxs.append(i)
+        
+        filtered_boxes = boxes[idxs, ...]
+        filtered_scores = scores[idxs, ...]
+        filtered_classes = classes[idxs, ...]
+        return filtered_boxes, filtered_scores, filtered_classes
+
+    def to_image_coords(self,boxes, height, width):
+        """
+        The original box coordinate output is normalized, i.e [0, 1].
+
+        This converts it back to the original coordinate based on the image
+        size.
+        """
+        box_coords = np.zeros_like(boxes)
+        box_coords[:, 0] = boxes[:, 0] * height
+        box_coords[:, 1] = boxes[:, 1] * width
+        box_coords[:, 2] = boxes[:, 2] * height
+        box_coords[:, 3] = boxes[:, 3] * width
+
+        return box_coords
+    def draw_bounding_box_on_image(self,image, bounding_box):
+        drawn_image = image.clone()
+
+        ymin,xmin,ymax, xmax = bounding_box
+        cv2.rectangle(drawn_image,(xmin,ymin),(xmax,ymax),(0,255,0),2)
+        cv2.putText(drawn_image,'Traffic Light',(x+w+10,y+h),0,0.3,(0,255,0))
+
+        return drawn_image
+
+    def draw_boxes(self,image, boxes, classes, thickness=4):
+        # Colors (one for each class)
+        
+#        cmap = ImageColor.colormap
+#        print("Number of colors =", len(cmap))
+#        COLOR_LIST = #sorted([c for c in cmap.keys()])
+#        print('COLOR_LIST: ', COLOR_LIST)
+        """Draw bounding boxes on the image"""
+        draw = ImageDraw.Draw(image)
+        for i in range(len(boxes)):
+            print('Drawing ', boxes [i,...])
+            bot, left, top, right = boxes[i, ...]
+            class_id = int(classes[i])
+            print("Classes: ", class_id)
+            color = self.color_list[class_id-i]
+            #COLOR_LIST[class_id-i]
+            print('color: ', type(color))
+            draw.line([(left, top), (left, bot), (right, bot), (right, top), (left, top)], width=thickness, fill=color)
+
+
 
   
   
@@ -132,7 +158,7 @@ def main(_):
 
 
     inference_engine = DetectionInferenceEngine()
-    image_path = './17938.png'
+    image_path = './out00046.png'
     image = Image.open(image_path)
     # the array based representation of the image will be used later in order to prepare the
     # result image with boxes and labels on it.
@@ -149,18 +175,18 @@ def main(_):
 
 
     print("Dict Result: ", output_dict)
-    confidence_cutoff = 0.8
+    confidence_cutoff = .02#0.8
     # Filter boxes with a confidence score less than `confidence_cutoff`
-    boxes, scores, classes = filter_boxes(confidence_cutoff, output_dict['detection_boxes'], output_dict['detection_scores'], output_dict['detection_classes'])
+#    boxes, scores, classes = filter_boxes(confidence_cutoff, output_dict['detection_boxes'], output_dict['detection_scores'], output_dict['detection_classes'])
 
     # The current box coordinates are normalized to a range between 0 and 1.
     # This converts the coordinates actual location on the image.
-    width, height = image.size
-    box_coords = to_image_coords(boxes, height, width)
+#    width, height = image.size
+#    box_coords = to_image_coords(boxes, height, width)
 
-    print('box coordinates: ', box_coords)
+    print('box coordinates: ', output_dict['detection_boxes'])
     #draw_bounding_box_on_image(image, box_coords)
-    draw_boxes(image, box_coords, classes, thickness=4)
+    inference_engine.draw_boxes(image, output_dict['detection_boxes'], output_dict['detection_classes'], thickness=4)
     image.save('inference_result.png')
     
     return
