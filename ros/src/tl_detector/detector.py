d6e79f36b611c2d99438353c85b4ea2127dec266
jlclemon <jlclemon@hotmail.com>
2018-03-24 21:15:05 -0500

    Started working on real detector code

--- ros/src/tl_detector/detector.py
+++ ros/src/tl_detector/detector.py
@@ -205,7 +205,7 @@ class SimDetector(BaseDetector):
 
         #Instantiate a inference engine
         # TODO Move thresh to parameter server
-        self.run_inference_engine = False
+        self.run_inference_engine = True
         self.inference_engine = DetectionInferenceEngine(model_path=model_path, confidence_thr= 0.02)
 
         # Resolve the path
@@ -427,12 +427,13 @@ class SimDetector(BaseDetector):
             # Returns a dictionaru with bounding boxes and classes
             output_dict,predicted_light_state =self.inference_engine.run_inference(detection_image_ex)
 
-
+            rospy.loginfo('Detection Scores:  - %s',output_dict['detection_scores']) 
+            rospy.logdebug
 
             # Just some debug output of the live boxes
-            print('predicted_light_state: ', predicted_light_state)
-            print('box coordinates: ', output_dict['detection_boxes'])
-            print('Detection Scores: ',output_dict['detection_scores'])
+            rospy.logdebug('predicted_light_state: %s', predicted_light_state)
+            rospy.logdebug('box coordinates: %s', output_dict['detection_boxes'])
+            rospy.logdebug('Detection Scores: %s',output_dict['detection_scores'])
             # draw the bounding boxes on the image
             detected_image = draw_bounding_boxes(detection_image, output_dict['detection_boxes'], output_dict['detection_classes'], self.inference_engine.color_list,thickness=4)
 
@@ -457,58 +458,277 @@ class SimDetector(BaseDetector):
 # "Real" Detector                                                             #
 #-----------------------------------------------------------------------------#
 
-#NOTE: Everything below here is pretty much garbage :/ Beware.
+#NOTE: Everything below here is beig transformed from error :/ Beware.
 
 class Detector(BaseDetector):
     '''
     '''
+    def __init__(self,vehicle_pose=None, waypoints=None, lights_config=None,
+                 save_path=None, model_path=None):
+        '''
+        Detector constructor
 
-    def __init__(self):
+        Takes all the normal parameters from the base class, plus:
+            save_path - <string> Directory location to save data to, None
+                        if you dont want to save any data
+        '''
+
+        # Call the super class init function
+        BaseDetector.__init__(self,
+                              vehicle_pose=vehicle_pose,
+                              waypoints=waypoints,
+                              lights_config=lights_config)
+
+        # Cache the next light/stop line for each waypoint
+        self.next_light = None
+
+        # Which waypoint are we closests to?
+        self.closest_wp = -1
+
+        # What was the last light wp and the state 
+        self.last_closest_light_wp = -1
+        self.last_closest_light_state = -1
+
+
+        # Do we want to gather training data?
+        self.save_data = False
+        self.save_path = save_path
+        self.save_count = 0
+
+        #Path to the model for inference
+        self.model_path = model_path
+
+        #Instantiate a inference engine
+        # TODO Move thresh to parameter server
+        self.run_inference_engine = False
+        self.inference_engine = DetectionInferenceEngine(model_path=model_path, confidence_thr= 0.02)
+
+        # Resolve the path
+        if(self.save_path != None):
+            self.save_path = abspath(expanduser(self.save_path))
+
+            # See if the path exists or makes sense. If it does then make
+            # the data collection directory to save images to
+            if(exists(self.save_path) and isfile(self.save_path)):
+                self.save_data = False
+                rospy.logwarn("Given path is an existing file. Image capture has been turned off.")
+            else:
+                self.save_path = self.save_path + "/images_" + datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+                mkpath(self.save_path)
+                self.save_data = True
+
+        # Subscribe to the ground truth data secretly here
+        rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray,
+                         self.vehicle_traffic_lights_cb, queue_size=1)
+
+    def set_vehicle_pose(self, pose):
+        '''
+        Set the current vehicle pose
+        '''
+        self.vehicle_pose = pose
+
+        # We cant determine the closest waypoint if we dont have any
+        if(self.waypoints is None):
+            return
+
+        # Set the vehicle's x, y, and yaw
+        v_x = pose.position.x
+        v_y = pose.position.y
+
+        # Determine closest waypoint
+        c_wp = -1
+        c_dist = -1
+        for i in range(0, len(self.waypoints)):
+            w_x = self.waypoints[i].pose.pose.position.x
+            w_y = self.waypoints[i].pose.pose.position.y
+            dist = sqrt((v_x - w_x)**2 + (v_y - w_y)**2)
+            if(dist < c_dist or c_dist == -1):
+                c_dist = dist
+                c_wp = i
+
+        # Set the closest waypoint
+        self.closest_wp = c_wp
+
+    def set_waypoints(self, waypoints):
+        '''
+        Set the current waypoint list
+        '''
+        self.waypoints = waypoints
+
+        # Once we have waypoints, try to calculate and cache the index of
+        # the next traffic light for each waypoint.
+        if(self.next_light is None):
+            self.set_next_lights()
+
+    def vehicle_traffic_lights_cb(self, msg):
+        '''
+        Callback for the ground truth traffic light data
+
+        msg.header.seq
+        msg.header.stamp.secs
+        msg.header.stamp.nsecs
+        msg.header.frame_id
+        msg.lights[].state [0=RED, 1=YELLOW, 2=GREEN, 4=UNKNOWN]
+        msg.lights[].pose.header.stamp.secs
+        msg.lights[].pose.header.stamp.nsecs
+        msg.lights[].pose.header.frame_id
+        msg.lights[].pose.pose.position.x
+        msg.lights[].pose.pose.position.y
+        msg.lights[].pose.pose.position.z
+        msg.lights[].pose.pose.orientation.x
+        msg.lights[].pose.pose.orientation.y
+        msg.lights[].pose.pose.orientation.z
+        msg.lights[].pose.pose.orientation.w
+        '''
+
+        # Set the up to date light statuses
+        self.lights = msg.lights
+
+        # Once we have lights, try to calculate and cache the index of
+        # the next traffic light for each waypoint.
+        if(self.next_light is None):
+            self.set_next_lights()
+            return
+
+
+        # Set final status:
+        #  - light_ind -> where to stop for next light
+        #  - light_state -> status of next light
+        # NOTE: if there's no know light, the "next light" waypoint is -1
+        # and we can set the status to unknown
+
+    def set_next_lights(self):
         '''
         '''
 
-        # Pulled this from the old code for now
-        self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
-        self.listener = tf.TransformListener()
-        self.last_light_state = TrafficLight.UNKNOWN
-        self.last_wp = -1
-        self.state_count = 0
-        self.state_count_threshold = 3
-        self.has_image = False
-        self.camera_image = None
+        # If we don't have waypoints or lights, we can't cache the next light
+        # per waypoint
+        if(self.waypoints is None or len(self.waypoints) == 0 or
+           self.lights is None or len(self.lights) == 0):
+            return
 
-    def image_cb(self, image):
-        """Identifies red lights in the incoming camera image and publishes the index
-            of the waypoint closest to the red light's stop line to /traffic_waypoint
+        # First, init the next_light structure
+        self.next_light = [[-1, -1, -1] for i in range(0, len(self.waypoints))]
 
-        Args:
-            msg (Image): image from car-mounted camera
+        # Next, we're going to find the closest waypoint to each stop light
+        l_closest = [-1 for i in range(0, len(self.lights))]
+        for i in range(0, len(self.lights)):
+            l_x = self.lights[i].pose.pose.position.x
+            l_y = self.lights[i].pose.pose.position.y
+            c_wp = -1
+            c_dist = -1
+            for j in range(0, len(self.waypoints)):
+                w_x = self.waypoints[j].pose.pose.position.x
+                w_y = self.waypoints[j].pose.pose.position.y
+                dist = sqrt((l_x - w_x) * (l_x - w_x) + (l_y - w_y) * (l_y - w_y))
 
-        """
-        self.has_image = True
-        self.image = image
-        light_wp = -1
-        state = None
-        light = None
+                if(c_dist == -1 or c_dist > dist):
+                    c_dist = dist
+                    c_wp = j
 
-        # List of positions that correspond to the line to stop in front of for a given intersection
-        stop_line_positions = self.config['stop_line_positions']
-        if(self.pose):
-            car_position = self.get_closest_waypoint(self.vehicle_pose)
+                if(c_dist == 0):
+                    break
 
-        # TODO find the closest visible traffic light (if one exists)
-        # ... light_wp = ?
+            l_closest[i] = c_wp
 
-        if light:
-            state = self.get_light_state(light)
+        # Next, find the closest waypoint to each traffic light stop line
+        sl_closest = [-1 for i in range(0, len(self.lights))]
+        for i in range(0, len(self.lights)):
+            sl_x = self.lights_config['stop_line_positions'][i][0]
+            sl_y = self.lights_config['stop_line_positions'][i][1]
 
+            wp = -1
+            wp_dist = -1
+            for j in range(0, len(self.waypoints)):
+                w_x = self.waypoints[j].pose.pose.position.x
+                w_y = self.waypoints[j].pose.pose.position.y
+                dist = sqrt((sl_x - w_x) * (sl_x - w_x) + (sl_y - w_y) * (sl_y - w_y))
+                if(wp_dist == -1 or dist < wp_dist):
+                    wp = j
+                    wp_dist = dist
+
+                if(wp_dist == 0):
+                    break
+
+            sl_closest[i] = wp
+
+        # Next, do a single pass on the waypoints to set traffic light
+        # and stop line locations
+        for i in range(0, len(l_closest)):
+            self.next_light[l_closest[i]][0] = i             # light index
+            self.next_light[l_closest[i]][1] = l_closest[i]  # light wp
+            self.next_light[l_closest[i]][2] = sl_closest[i] # stop line wp
+
+        # Next, fill in the spots in between by going backwards
+        n_l = -1
+        n_wp = -1
+        n_sl = -1
+        for i in reversed(range(0, len(self.next_light))):
+            if(self.next_light[i][0] != -1):
+                n_l  = self.next_light[i][0]
+                n_wp = self.next_light[i][1]
+                n_sl = self.next_light[i][2]
+            else:
+                self.next_light[i][0] = n_l
+                self.next_light[i][1] = n_wp
+                self.next_light[i][2] = n_sl
+
+    def set_image(self, image):
         '''
-        Publish upcoming red lights at camera frequency.
-        Each predicted state has to occur `STATE_COUNT_THRESHOLD` number
-        of times till we start using it. Otherwise the previous stable state is
-        used.
+        Override the set_image() function so we can do data gather potentially
+        when an image comes in
         '''
+
+        # Grab the image - convert it to something we can use and save
+        self.image = self.bridge.imgmsg_to_cv2(image, "bgr8")
+
+        # We can't do anything here if we don't have enough info
+        if(self.vehicle_pose is None or self.waypoints is None or self.lights is None):
+            return
+
+        # TODO: Determine if what we're looking at is the next up light
+        #       and grab the state if it is. If we don't, we're going to
+        #       get mostly pictures of hills...
+
+
+        if self.run_inference_engine:
+            # Grab the detector result image
+            # TODO: add in the marking of the image
+            # I plan on using the simulated version so the vehicle can run
+            detection_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+
+            # The model expects batches so ie 4 dimensional data so add in a single dimension to
+            # get a bactch size of 1
+            detection_image_ex = np.expand_dims(detection_image, axis=0)
+
+            # Returns a dictionaru with bounding boxes and classes
+            output_dict,predicted_light_state =self.inference_engine.run_inference(detection_image_ex)
+
+            rospy.loginfo('Detection Scores:  - %s',output_dict['detection_scores']) 
+
+            # Just some debug output of the live boxes
+            print('predicted_light_state: ', predicted_light_state)
+            print('box coordinates: ', output_dict['detection_boxes'])
+            print('Detection Scores: ',output_dict['detection_scores'])
+            # draw the bounding boxes on the image
+            detected_image = draw_bounding_boxes(detection_image, output_dict['detection_boxes'], output_dict['detection_classes'], self.inference_engine.color_list,thickness=4)
+
+            # Convert the detection image to an image message and store for the publish loop
+            self.detector_result_image_msg = self.bridge.cv2_to_imgmsg(detected_image, 'rgb8')#encoding="passthrough")
+
+        # No need to do this if we're not saving any data
+        if(not self.save_data):
+            return
+
+        # If we don't know where we are, we cant update the light status
+        if(self.closest_wp == -1):
+            return
+
+        rospy.loginfo('Predicted Light State - %s for light at ', 
+            predicted_light_state,self.closest_wp)
+
+
+        #I do like the temporal filtering
         if self.light_state != state:
             self.state_count = 0
             self.light_state = state
@@ -521,41 +741,22 @@ class Detector(BaseDetector):
             self.upcoming_red_light_pub.publish(Int32(self.last_wp))
         self.state_count += 1
 
-    def get_closest_waypoint(self, pose):
-        """Identifies the closest path waypoint to the given position
-            https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
-        Args:
-            pose (Pose): position to match a waypoint to
 
-        Returns:
-            int: index of the closest waypoint in self.waypoints
 
-        """
-        #TODO implement
-        return 0
+        if(self.next_light[self.closest_wp][1] != -1):
+            self.light_ind = self.next_light[self.closest_wp][2]
+            self.light_state = self.lights[self.next_light[self.closest_wp][0]].state
+        else:
+            self.light_ind = -1
+            self.light_state = TrafficLight.UNKNOWN
 
-    def get_light_state(self, light):
-        """Determines the current color of the traffic light
 
-        Args:
-            light (TrafficLight): light to classify
-
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
-        """
-        if(not self.has_image):
-            self.prev_light_loc = None
-            return False
-
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-
-        # Get classification
-        return self.light_classifier.get_classification(cv_image)
+        # Finally, save the image
+        file_name = self.save_path + "/"+"img_" + str(self.save_count) + "_s" + str(self.light_state) + ".png"
+        cv2.imwrite(file_name, self.image)
+        self.save_count += 1
 
     def traffic_light_status(self):
         '''
         '''
         return self.light_ind, self.light_state
-
-
